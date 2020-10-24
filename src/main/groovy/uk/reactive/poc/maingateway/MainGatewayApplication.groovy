26491ba33a6743245af286bcaf03f1b44e19b036
Marcin Cichocki <marcin@cichocki.co.uk>
2020-10-24 18:41:02 +0100

    initializr commit

---
+++ src/main/groovy/uk/reactive/poc/maingateway/MainGatewayApplication.groovy
@@ -0,0 +1,13 @@
+package uk.reactive.poc.maingateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MainGatewayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MainGatewayApplication, args)
+	}
+
+}
---
+++ src/test/groovy/uk/reactive/poc/maingateway/MainGatewayApplicationTests.groovy
@@ -0,0 +1,13 @@
+package uk.reactive.poc.maingateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
